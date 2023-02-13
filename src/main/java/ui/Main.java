e3f54259c05ff5354a4cb9e56db1d265ad5f583e
Emil Olsen <emiols2003@skola.goteborg.se>
2023-02-13 22:04:41 +0100

    Fixed error handling in main() and fixed the code structure

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -6,31 +6,35 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner input = new Scanner(System.in);
-        GameEngine game = Launcher.gameStartup(input);
-        while (true)
-        {
-            System.out.printf("Get ready %s press enter when ready", game.getCurrentPlayer().getName());
-            input.nextLine();
-            while (true)
-            {
-                Launcher.newQuestion(game);
-                game.answerQuestion(input.nextLine());
-                if (game.checkForPlayerSwap())
-                {
-                    game.swapCurrentPlayer();
-                    break;
-                }
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            GameEngine game = Launcher.gameStartup(input);
+            while (!game.checkForGameEnd()) {
+                initiateRound(input, game);
+                manageQuestions(input, game);
+                game.newRoundCheckAndSet();
             }
-            Launcher.clearScreen();
-            game.newRoundCheckAndSet();
-            if (game.checkForGameEnd())
-            {
-                Launcher.declareWinner(game);
+            Launcher.declareWinner(game);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void initiateRound(Scanner input, GameEngine game) {
+        System.out.printf("Get ready %s. Press enter when ready", game.getCurrentPlayer().getName());
+        input.nextLine();
+    }
+
+    public static void manageQuestions(Scanner input, GameEngine game) {
+        while (true) {
+            Launcher.newQuestion(game);
+            game.answerQuestion(input.nextLine());
+            if (game.checkForPlayerSwap()) {
+                game.swapCurrentPlayer();
                 break;
             }
-
+            Launcher.clearScreen();
         }
     }
 }
