065ed1b0b3942499562a844c647154cb5d12448c
Emil Olsen <emiols2003@skola.goteborg.se>
2023-02-13 17:39:49 +0100

    Added first test

---
+++ src/test/java/core/TextFileOpsTest.java
@@ -0,0 +1,16 @@
+package core;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class TextFileOpsTest {
+    @Test
+    public void testCreateAnswerMapWithBadFile() {
+        assertThrows(IOException.class, () -> {
+            TextFileOps.createAnswerMap("na");
+        }, "IOException expected");
+
+    }
+}
