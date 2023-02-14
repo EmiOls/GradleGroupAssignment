2063a49cc2ad695367acb5da189d9713768d5832
Saman Amirrahmani <samami2002@skola.goteborg.se>
2023-02-14 05:36:46 +0100

    Added unit tests for Launcher

--- src/main/java/ui/Launcher.java
+++ src/main/java/ui/Launcher.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 
 public class Launcher {
 
-    //This method asks the user for player names, number of rounds then makes an instance of GameEngine and returns it.
+    // This method asks the user for player names, number of rounds then makes an
+    // instance of GameEngine and returns it.
     public static GameEngine gameStartup(Scanner input) throws IOException {
         System.out.println("-----Question Game v0.1a-----");
         System.out.print("Player 1 name: ");
@@ -30,32 +31,31 @@ public class Launcher {
         return game;
     }
 
-    //This method is used to check if the entered number of rounds are allowed.(Min:1 Max:4)
-    public static boolean checkRounds(int rounds)
-    {
+    // This method is used to check if the entered number of rounds are
+    // allowed.(Min:1 Max:4)
+    public static boolean checkRounds(int rounds) {
         if (rounds >= 1 && rounds <= 4) {
             return true;
         }
         return false;
     }
 
-    //This method declares the winner.
+    // This method declares the winner.
     public static void declareWinner(GameEngine game) {
         Player winner = game.getResult();
         if (winner.getName().equals("TIE")) {
-            System.out.printf("Game was a tie you both scored %d", game.getPlayerOne().getScore());
+            System.out.printf("Game was a tie, you both scored: %d", game.getPlayerOne().getScore());
         } else {
-            System.out.printf("%s Won with %d points", winner.getName(), winner.getScore());
+            System.out.printf("%s won with %d points!", winner.getName(), winner.getScore());
         }
     }
 
-    //This method prints a new question.
-    public static void newQuestion(GameEngine game)
-    {
+    // This method prints a new question.
+    public static void newQuestion(GameEngine game) {
         System.out.printf("What is the capital of %s \n", game.getCountry());
     }
-    public static void clearScreen()
-    {
+
+    public static void clearScreen() {
         System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
